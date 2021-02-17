ab7e0cf2e160026233e9b7d1879b2cf86668684c
DKlaver <dklaver15@gmail.com>
2021-02-17 11:04:42 -0600

    Added import for title bar and added title bar to render.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
+import TitleBar from './TitleBar/titlebar';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
 		this.music = [];
+	}
+
+	render() {
+		return (
+			<div className="container-fluid">
+				<TitleBar />
+			</div>
+		)
 	}
 }
