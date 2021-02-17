1203ef8178d0a13c9551f550e3624fc201720dee
DKlaver <dklaver15@gmail.com>
2021-02-17 13:19:52 -0600

    Renamed component to APIConnect and added ConnectToAPI method to call the API.

---
+++ src/components/ConnectToAPI/ConnectToAPI.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+const $ = require( "jquery" )
+
+class ConnectToAPI extends Component {
+	constructor(props) {
+		super(props);
+		this.muic=[];
+	}
+
+	ConnectToAPI() {
+		var settings = {
+			"url": "http://www.devcodecampmusiclibrary.com/api/music",
+			"method": "GET",
+			"timeout": 0,
+		 };
+		 
+		 $.ajax(settings).done(function (response) {
+			console.log(response);
+		 });
+	}
+
+	render() {
+		return (
+			<this.ConnectToAPI />
+		);
+	}
+}
+
+export default ConnectToAPI;
