4b591ec8e4c342081f3c62d2f492d2d34d519ca6
DKlaver <dklaver15@gmail.com>
2021-02-17 13:21:15 -0600

    Added call of APIConnect.

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TitleBar from './TitleBar/titleBar';
 import Footer from './Footer/footer';
+import APIConnect from './APIConnect/APIConnect';
 
 class App extends Component {
 	constructor(props) {
@@ -12,6 +13,7 @@ class App extends Component {
 		return (
 			<div className="container-fluid">
 				<TitleBar />
+					<APIConnect />
 				<Footer />
 			</div>
 		)
